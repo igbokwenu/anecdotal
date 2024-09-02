ce8bf2b5d07c0c8603b7b557a45141426ec7b6f3
Increase <okechukwu@habilisfusion.co>
2024-09-02 19:05:25 +0100

    Improving voice chat icon UI

--- lib/widgets/microphone.dart
+++ lib/widgets/microphone.dart
@@ -1,6 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class MicrophoneIconWidget extends StatefulWidget {
   final double size;
@@ -32,21 +32,33 @@ class MicrophoneIconWidgetState extends State<MicrophoneIconWidget> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _toggleListening,
-      child: _isListening
-          ? Pulse(
-              infinite: true,
-              duration: const Duration(milliseconds: 2000),
-              child: Icon(
-                Icons.mic,
-                color: Theme.of(context).colorScheme.secondary,
-                size: widget.size,
+      child: SizedBox(
+        width: widget.size * 2,
+        height: widget.size * 2,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (_isListening)
+              SpinKitRipple(
+                color: Theme.of(context).colorScheme.secondary.withOpacity(0.6),
+                size: widget.size * 2,
               ),
-            )
-          : Icon(
-              Icons.mic_none,
-              color: Theme.of(context).iconTheme.color,
-              size: widget.size,
+            Center(
+              child: Pulse(
+                infinite: _isListening,
+                duration: const Duration(milliseconds: 2000),
+                child: Icon(
+                  _isListening ? Icons.stop_circle_rounded : Icons.mic_none,
+                  color: _isListening
+                      ? Theme.of(context).colorScheme.secondary
+                      : Theme.of(context).iconTheme.color,
+                  size: widget.size,
+                ),
+              ),
             ),
+          ],
+        ),
+      ),
     );
   }
 }
